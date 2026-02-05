b7a66a5335052dc20d525682c2badc40616e8d7c
skawagoe-bit <skawagoe-bit@users.noreply.github.com>
2026-02-05 14:45:50 +0900

    Fix empty interface lint error in Input component

--- components/ui/input.tsx
+++ components/ui/input.tsx
@@ -5,6 +5,7 @@ import { cn } from "@/lib/utils"
 export interface InputProps
   extends React.InputHTMLAttributes<HTMLInputElement> {
     // Add custom props here if needed
+    _?: never;
 }
 
 const Input = React.forwardRef<HTMLInputElement, InputProps>(
